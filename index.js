51d97be29e0e2c02ec471615902358864cf3e6b9
raresmigea <raresini@yahoo.com>
2020-04-04 14:55:37 +0300

    express routes and middleware

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi test");
+const express = require("express");
+
+const app = express();
+
+//middleware - functions that executes when routes are being hit
+app.use('/posts', (req, res) => {
+	console.log('this is middleware');
+});
+
+//routes
+app.get('/', (req, res) => {
+	res.send('we are at home');
+});
+
+app.get('/posts', (req, res) => {
+	res.send('we are at posts');
+});
+
+
+app.listen(3000);
