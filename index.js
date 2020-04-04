67c4149ba3d274cf7d2dfaa5e6cf0327ec0cfd25
raresmigea <raresini@yahoo.com>
2020-04-04 20:42:23 +0300

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+//connect to database
+mongoose.connect(process.env.DB_CONNECTION,
+	{ useNewUrlParser: true },
+	() => console.log("connected to database!"));
+
 
 //middleware - functions that executes when routes are being hit
 app.use('/posts', (req, res) => {
